8cabac8e0fd41fc79de18dda68749b2024ebe059
Robert  Alexander <delrobert.alexander@gmail.com>
2023-08-25 15:50:14 -0300

    Aplicando o tipo de varaivel double, para uso de casas decimáis.

---
+++ operacao-numeros-decimais/src/notas1/MediaDoAluno.java
@@ -0,0 +1,25 @@
+package notas1;
+
+public class MediaDoAluno {
+
+	public static void main(String[]args) {
+		
+		String nomeAluno = "José Erminio";
+		String matriculaAluno = "42517841";
+		double nota1 = 7.4;
+		double nota2 = 4.5;
+		double nota3 = 5.8;
+		double nota4 = 8;
+		
+		double media = (nota1 + nota2 + nota3 + nota4)/4;
+		
+		System.out.println("Nome do Aluno = " + nomeAluno);
+		System.out.println("Matricula do Aluno = " + matriculaAluno);
+		System.out.println("Media do Aluno = " + media);
+		System.out.println("O aluno " + nomeAluno + " foi aprovado!!");
+		
+		
+		
+		
+	}
+}
